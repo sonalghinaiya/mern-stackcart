8217a106b6ce562797172341588ee0135ed241a2
Sonal Ghinaiya <ghinaiyasonal1308@gmail.com>
2025-12-10 16:50:03 +0530

    feat: add basic user CRUD API with MongoDB and Express - Added Mongoose connection to local MongoDB - Created User model with schema & timestamps - Implemented routes:   GET /users → returns HTML list   GET /api/users → returns JSON users   POST /api/user → creates new user with validation

---
+++ index.js
@@ -0,0 +1,91 @@
+import express from "express";
+import mongoose from "mongoose";
+
+const app = express();
+const PORT = process.env.PORT;
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/user-crud")
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log("MongoDB Error", err));
+
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    gender: {
+      type: String,
+    },
+    jobTitle: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("user", userSchema);
+
+app.use(express.json());
+
+app.get("/users", async (req, res) => {
+  const allDbUsers = await User.find({});
+  const html = `
+  <ul>
+${allDbUsers
+  .map((user) => `<li>${user.firstName} - ${user.email}</li>`)
+  .join("")}
+    </ul>
+  `;
+  return res.send(html);
+});
+
+app.get("/api/users", async (req, res) => {
+  const allDbUsers = await User.find({});
+  return res.json(allDbUsers);
+});
+app.post("/api/user", async (req, res) => {
+  const body = req.body;
+  if (
+    !body ||
+    !body.firstName ||
+    !body.lastName ||
+    !body.email ||
+    !body.password ||
+    !body.gender ||
+    !body.jobTitle
+  ) {
+    return res.status(400).json({ message: "All fiels are required..." });
+  }
+
+  const result = await User.create({
+    firstName: body.firstName,
+    lastName: body.lastName,
+    email: body.email,
+    password: body.password,
+    gender: body.gender,
+    jobTitle: body.jobTitle,
+  });
+
+  return res.status(201).json({
+    message: "Success",
+    data: result,
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port http://localhost:${PORT}`);
+});
