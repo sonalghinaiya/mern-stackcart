6657b5cc93f6de90520c462d0f8c80046642b1f8
Sonal Ghinaiya <ghinaiyasonal1308@gmail.com>
2026-01-08 11:33:23 +0530

    feat(ai): integrate Google Generative AI and update environment variables

--- services/aiService.js
+++ services/aiService.js
@@ -1,24 +1,60 @@
-import OpenAI from "openai";
+// import OpenAI from "openai";
+import { GoogleGenerativeAI } from "@google/generative-ai";
 
-const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
-});
+// const openai = new OpenAI({
+//   apiKey: process.env.OPENAI_API_KEY,
+// });
+
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
 export const getAIResponse = async (message) => {
-  const response = await openai.chat.completions.create({
-    // model: "gpt-4o-mini",
-    model: "gpt-3.5-turbo",
-    messages: [
-      {
-        role: "system",
-        content: "You are a helpful assistant for an e-commerce backend API"
-      },
-      {
-        role: "user",
-        content: message,
-      },
-    ],
+  const model = genAI.getGenerativeModel({
+    model: "gemini-2.5-flash",
   });
-  console.log("--", response.choices[0].message.content);
-  return response.choices[0].message.content;
+  const result = await model.generateContent(message);
+  return result.response.text();
 };
+
+// const gemini_api_key = process.env.GEMINI_API_KEY;
+// const googleAI = new GoogleGenerativeAI(gemini_api_key);
+// const geminiConfig = {
+//   temperature: 0.9,
+//   topP: 1,
+//   topK: 1,
+//   maxOutputTokens: 4096,
+// };
+
+// const geminiModel = googleAI.getGenerativeModel({
+//   model: "gemini-pro",
+//   geminiConfig,
+// });
+
+// export const getAIResponse = async (message) => {
+//   try {
+//     const prompt = "Tell me about google.";
+//     const result = await geminiModel.generateContent(prompt);
+//     const response = result.response;
+//     console.log(response.text());
+//   } catch (error) {
+//     console.log("response error", error);
+//   }
+// };
+
+// export const getAIResponse = async (message) => {
+//   const response = await openai.chat.completions.create({
+//     // model: "gpt-4o-mini",
+//     model: "gpt-3.5-turbo",
+//     messages: [
+//       {
+//         role: "system",
+//         content: "You are a helpful assistant for an e-commerce backend API"
+//       },
+//       {
+//         role: "user",
+//         content: message,
+//       },
+//     ],
+//   });
+//   console.log("--", response.choices[0].message.content);
+//   return response.choices[0].message.content;
+// };
