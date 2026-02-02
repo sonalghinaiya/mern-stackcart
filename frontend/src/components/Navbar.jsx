ee50c9700f35d4fd49ae0912ca56dda4a57fb1a6
Sonal Ghinaiya <ghinaiyasonal1308@gmail.com>
2026-02-02 14:00:30 +0530

    feat(Navbar): enhance styling and structure of Navbar component

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,18 +5,37 @@ function Navbar() {
   const navigate = useNavigate();
   const token = localStorage.getItem("token");
 
+  if (!token) return null;
+
   const logout = () => {
     localStorage.clear();
     navigate("/login");
   };
   return (
-    <nav className="flex justify-around m-5 p-6">
-      <Link to="/products">Products</Link>
-      <Link to="/products/add">Add Product</Link>
-      <Link to="/profile">Profile</Link>
-      <Link to="/login">Login</Link>
-      <Link to="/register">Register</Link>
-      <button onClick={logout}>Logout</button>
+    <nav className="bg-gray-800 text-white flex justify-between items-center py-3 px-6">
+      <div className="flex items-center gap-8">
+        <Link to="/products" className="hover:text-gray-300">
+          Products
+        </Link>
+        <Link to="/products/add" className="hover:text-gray-300">
+          Add Product
+        </Link>
+        <Link to="/profile" className="hover:text-gray-300">
+          Profile
+        </Link>
+        <Link to="/login" className="hover:text-gray-300">
+          Login
+        </Link>
+        <Link to="/register" className="hover:text-gray-300">
+          Register
+        </Link>
+        <button
+          onClick={logout}
+          className="bg-red-500 px-3 py-1 rounded hover:bg-red-800"
+        >
+          Logout
+        </button>
+      </div>
     </nav>
   );
 }
