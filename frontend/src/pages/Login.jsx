67c33a6268433f77733c10d7ad285585dab1c45b
Sonal Ghinaiya <ghinaiyasonal1308@gmail.com>
2026-02-03 12:36:40 +0530

    feat(Login, Register): enhance UI and add navigation links for user authentication forms

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import api from "../api/axios";
 
 function Login() {
@@ -25,22 +25,44 @@ function Login() {
     }
   };
   return (
-    <div>
-      <h2>Login</h2>
-      <form action="" onSubmit={handleSubmit}>
+    <div className="flex items-center justify-center bg-gray-100 min-h-screen">
+      <form
+        onSubmit={handleSubmit}
+        className="bg-white p-6 rounded shadow w-full max-w-md space-y-4"
+      >
+        <h2 className="text-xl font-bold text-center">Login</h2>
+        <p className="text-sm text-gray-500 text-center mb-4">
+          Sign in to your account
+        </p>
         <input
           type="email"
           placeholder="Email"
           value={email}
+          className="input"
           onChange={(e) => setEmail(e.target.value)}
         />
         <input
           type="password"
           placeholder="Password"
           value={password}
+          className="input"
           onChange={(e) => setPassword(e.target.value)}
         />
-        <button type="submit">Login</button>
+        <p className="text-sm text-right text-blue-600 cursor-pointer hover:underline">
+          Forgot your password?
+        </p>
+        <p className="text-sm text-center mt-4">
+          Don't have an account?{" "}
+          <Link to="/register" className="text-blue-600 hover:underline">
+            Sign Up
+          </Link>{" "}
+        </p>
+        <button
+          type="submit"
+          className="bg-blue-600 text-white rounded w-full py-2"
+        >
+          Login
+        </button>
       </form>
     </div>
   );
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import api from "../api/axios";
 
 function Register() {
@@ -17,24 +17,72 @@ function Register() {
     }
   };
   return (
-    <div>
-      <h2>Register</h2>
-      <form action="" onSubmit={handleSubmit}>
-        <input type="text" name="firstName" placeholder="Firstname" />
-        <input type="text" name="lastName" placeholder="Lastname" />
-        <input type="email" name="email" placeholder="email" />
-        <input type="password" name="password" placeholder="password" />
-        <input type="text" name="jobTitle" placeholder="Job Title" />
-        <label>
-          <input type="radio" name="gender" value="Male" />
-          Male
-        </label>
-        <label>
-          <input type="radio" name="gender" value="Female" />
-          Female
+    <div className="flex items-center justify-center bg-gray-100 min-h-screen">
+      <form
+        onSubmit={handleSubmit}
+        className="bg-white p-6 rounded shadow w-full max-w-md space-y-4"
+      >
+        <h2 className="text-xl font-bold text-center">Register</h2>
+        <p className="text-gray-500 text-sm text-center mb-4">
+          Create your account to get started
+        </p>
+        <input
+          className="input"
+          type="text"
+          name="firstName"
+          placeholder="First name"
+        />
+        <input
+          className="input"
+          type="text"
+          name="lastName"
+          placeholder="Last name"
+        />
+        <input
+          className="input"
+          type="email"
+          name="email"
+          placeholder="Email"
+        />
+        <input
+          className="input"
+          type="password"
+          name="password"
+          placeholder="Password"
+        />
+        <input
+          className="input"
+          type="text"
+          name="jobTitle"
+          placeholder="Job Title"
+        />
+
+        <div className="flex gap-4 mt-2">
+          <label className="flex items-center gap-1">
+            <input type="radio" name="gender" value="Male" />
+            Male
+          </label>
+          <label className="flex items-center gap-1">
+            <input type="radio" name="gender" value="Female" />
+            Female
+          </label>
+        </div>
+        <label className="text-sm text-gray-600 mt-2 block">
+          Profile image
         </label>
         <input type="file" name="profileImage" />
-        <button type="submit">Register</button>
+        <p className="text-sm text-center mt-4">
+          Already have an account?{" "}
+          <Link to="/login" className="text-blue-600 hover:underline">
+            Login
+          </Link>
+        </p>
+        <button
+          className="w-full bg-blue-600 text-white py-2 rounded"
+          type="submit"
+        >
+          Register
+        </button>
       </form>
     </div>
   );
