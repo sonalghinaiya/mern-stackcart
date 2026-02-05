ce887f37b26711a04335268e151264413f78c520
Sonal Ghinaiya <ghinaiyasonal1308@gmail.com>
2026-02-05 13:52:59 +0530

    chore: update import statement for cloudinary configuration and add missing environment variables

--- backend/middlewares/upload.js
+++ backend/middlewares/upload.js
@@ -1,6 +1,6 @@
 import multer from "multer";
 import { CloudinaryStorage } from "multer-storage-cloudinary";
-import cloudinary from "../config/cloudinary";
+import cloudinary from "../config/cloudinary.js";
 
 const userStorage = new CloudinaryStorage({
   cloudinary,
