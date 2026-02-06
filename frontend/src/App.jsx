401681e6fe96a837044221acf51663f64c8ecd73
Sonal Ghinaiya <ghinaiyasonal1308@gmail.com>
2026-02-06 12:53:50 +0530

    fix: correct root route element to redirect to login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,7 +13,7 @@ function App() {
     <BrowserRouter>
       <Navbar />
       <Routes>
-        <Route path="/" element="" />
+        <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route
