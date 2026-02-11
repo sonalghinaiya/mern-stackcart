1e5c42ae25c181f91a4fe0432e295c1840a9058c
Sonal Ghinaiya <ghinaiyasonal1308@gmail.com>
2026-02-11 22:16:00 +0530

    fix(smtp): update transporter configuration to use Brevo SMTP settings

--- backend/utils/mail/gmail.js
+++ backend/utils/mail/gmail.js
@@ -12,50 +12,12 @@ import nodemailer from "nodemailer";
 // });
 
 export const transporter = nodemailer.createTransport({
-  host: "smtp-relay.gmail.com",
+  host: "smtp-relay.brevo.com",
   port: 587,
   secure: false,
   auth: {
-    user: process.env.GMAIL_USER,
-    pass: process.env.GMAIL_APP_PASSWORD,
-  },
-  tls: {
-    rejectUnauthorized: false,
+    user: process.env.BREVO_USER,
+    pass: process.env.BREVO_PASS,
   },
 });
-
-
-// export const transporter = nodemailer.createTransport({
-//   host: "smtp.gmail.com",
-//   port: 587, 
-//   secure: false,
-//   auth: {
-//     user: process.env.GMAIL_USER,
-//     pass: process.env.GMAIL_APP_PASSWORD,
-//   },
-//   requireTLS: true,
-//   tls: {
-//     family: 4,
-//     rejectUnauthorized: false,
-//   },
-//   connectionTimeout: 10000,
-//   greetingTimeout: 10000,
-// });
-
-// import nodemailer from "nodemailer";
-
-// export const transporter = nodemailer.createTransport({
-//   host: "smtp.gmail.com",
-//   port: 465,          
-//   secure: true,     
-
-//   auth: {
-//     user: process.env.GMAIL_USER,
-//     pass: process.env.GMAIL_APP_PASSWORD,
-//   },
-
-//   tls: {
-//     family: 4,      
-//   },
-// });
 
