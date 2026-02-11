382f90fce98b8e28177dfe7ddadb47eed5b8f59b
Sonal Ghinaiya <ghinaiyasonal1308@gmail.com>
2026-02-11 14:25:57 +0530

    fix(smtp): update Gmail transporter configuration to use host and port settings

--- backend/utils/mail/gmail.js
+++ backend/utils/mail/gmail.js
@@ -1,12 +1,27 @@
+// import nodemailer from "nodemailer";
+
+// export const transporter = nodemailer.createTransport({
+//   service: "gmail",
+//   auth: {
+//     user: process.env.GMAIL_USER,
+//     pass: process.env.GMAIL_APP_PASSWORD,
+//   },
+//   tls: {
+//     rejectUnauthorized: false,
+//   },
+// });
+
 import nodemailer from "nodemailer";
 
 export const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.GMAIL_USER,
     pass: process.env.GMAIL_APP_PASSWORD,
   },
   tls: {
-    rejectUnauthorized: false,
+    family: 4,
   },
 });
